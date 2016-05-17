e64c7f124330f11ba9462b7fdb8f0eabebff4e84
Liam Ellis <liamhellis@gmail.com>
2016-05-17 16:55:05 -0700

    days end push

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,15 +14,13 @@ public class App {
         return new ModelAndView(model, "templates/layout.vtl");
       }, new VelocityTemplateEngine());
 
-      get("/profile", (request, response) -> {
+      get("/signUp", (request, response) -> {
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("template", "templates/profile.vtl");
         return new ModelAndView(model, "templates/layout.vtl");
       }, new VelocityTemplateEngine());
   }
 
-  public static Boolean methodName(String userInput) {
-    return true;
-  }
+
 
 }
