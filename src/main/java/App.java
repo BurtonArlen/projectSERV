304fd29efecf4f2d6fb8d24008adb315eac14658
DariusCapizzi <DariusCapizzi@gmail.com>
2016-05-18 10:14:31 -0700

    updating frontend

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,17 +14,13 @@ public class App {
         return new ModelAndView(model, "templates/layout.vtl");
       }, new VelocityTemplateEngine());
 
-      // get("/detector", (request, response) -> {
-      //   Map<String, Object> model = new HashMap<String, Object>();
-      //
-      //   String userInput = request.queryParams("blank");
-      //   App newApp = new App();
-      //   Boolean results = newApp.methodName(userInput);
-      //   model.put("results", results);
-      //
-      //   model.put("template", "templates/detector.vtl");
-      //   return new ModelAndView(model, "templates/layout.vtl");
-      // }, new VelocityTemplateEngine());
-      //
+      get("/signUp", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/signUp.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
   }
+
+
+
 }
